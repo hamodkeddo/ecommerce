21ec7fd268fac1bc203400fa854776ae40b6d94f
hamodkeddo <hamodkeddo@gmail.com>
2026-03-24 20:58:40 +0300

    fixed Auth import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes,Route } from "react-router-dom"
 import Home from "./pages/Home"
-import Auth from "./pages/auth"
+import Auth from "./pages/Auth"
 import Checkout from "./pages/Checkout"
 import Navbar from "./components/Navbar";
 
