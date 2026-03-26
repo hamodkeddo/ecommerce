7dbee0db7fe9ecf1f12c0c4327a1f4710460c3b7
hamodkeddo <hamodkeddo@gmail.com>
2026-03-26 13:02:23 +0300

    navigate to home after sign in fixed

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
-import {useForm ,useNavigate} from 'react-hook-form';
+import {useForm } from 'react-hook-form';
 import { AuthContext } from "../context/AuthContext";
+import {useNavigate}from 'react-router-dom'
 export default function Auth(){
     const [mode , setMode]=useState("signup")
     const [error,setError]=useState(null)
