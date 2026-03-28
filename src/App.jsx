4a304ad60827daf99bc8679fd1e41338cd061372
hamodkeddo <hamodkeddo@gmail.com>
2026-03-28 20:11:31 +0300

    product details feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import './App.css'
-import { Routes,Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Auth from "./pages/Auth"
-import Checkout from "./pages/Checkout"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
 import Navbar from "./components/Navbar";
-import AuthProvider from './context/AuthContext'
-import ProductDetails from "./pages/ProductDetails"
+import AuthProvider from "./context/AuthContext";
+import ProductDetails from "./pages/ProductDetails";
 function App() {
-
   return (
-   <AuthProvider>
-     <div className="app ">
-        <Navbar/>
+    <AuthProvider>
+      <div className="app ">
+        <Navbar />
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/auth" element={<Auth/>}/>
-            <Route path="/checkout" element={<Checkout/>}/>
-            <Route path="/products/:id" element={<ProductDetails/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
         </Routes>
-     </div>
-   </AuthProvider>
-  )
+      </div>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,56 +1,58 @@
 import { createContext, useContext, useState } from "react";
 
-const AuthContext = createContext(null)
+const AuthContext = createContext(null);
 
-export default function ContextProvider({children}){
-    const [user,setUser] = useState(
-        localStorage.getItem("currentUserEmail")
-        ? {email:localStorage.getItem("currentUserEmail")}
-        :null
+export default function ContextProvider({ children }) {
+  const [user, setUser] = useState(
+    localStorage.getItem("currentUserEmail")
+      ? { email: localStorage.getItem("currentUserEmail") }
+      : null,
+  );
+
+  function signUp(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    if (users.find((u) => u.email === email)) {
+      return { success: false, error: "email already exists" };
+    }
+
+    const newUser = { email, password };
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUserEmail", email);
+
+    setUser({ email });
+
+    return { success: true };
+  }
+
+  function login(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    const user = users.find(
+      (u) => u.email === email && u.password === password,
     );
 
-    function signUp(email,password){
-        const users = JSON.parse(localStorage.getItem("users") || "[]" )
-        if (users.find(((u)=>u.email===email))){
-            return {success:false, error:"email already exists"}
-        }
-
-        const newUser = {email,password};
-        users.push(newUser)
-        localStorage.setItem("users", JSON.stringify(users));
-        localStorage.setItem("currentUserEmail",email);
-
-        setUser({email})
-
-        return {success:true};
+    if (!user) {
+      return { success: false, error: "invalid email or password" };
     }
+    localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
+    return { success: true };
+  }
 
-    function login(email,password){
-        const users = JSON.parse(localStorage.getItem("users") || "[]" )
-        const user =users.find(
-            u=>u.email===email && u.password===password
-        );
+  function logout() {
+    localStorage.removeItem("currentUserEmail");
+    setUser(null);
+  }
 
-        if(!user){
-            return {success: false, error: "invalid email or password"};
-        }
-        localStorage.setItem("currentUserEmail",email);
-        setUser({email})
-        return{success:true};
-    }
-
-    function logout(){
-        localStorage.removeItem("currentUserEmail")
-        setUser(null)
-    }
-
-    return(
-         <AuthContext.Provider value={{signUp,user,logout,login}}>{children}</AuthContext.Provider>
-        );
+  return (
+    <AuthContext.Provider value={{ signUp, user, logout, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export function useAuth(){
-    const context = useContext(AuthContext)
+export function useAuth() {
+  const context = useContext(AuthContext);
 
-    return context
+  return context;
 }
