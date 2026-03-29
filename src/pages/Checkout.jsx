76ac616bc5896852ef365c0914f8f8c98235e420
hamodkeddo <hamodkeddo@gmail.com>
2026-03-29 23:22:53 +0300

    cart context added

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,3 +1,16 @@
-export default function Checkout(){
-    return <div>checkout</div>
+import { useCart } from "../context/CartContext";
+
+export default function Checkout() {
+  return (
+    <div className="page">
+      <div className="container">
+        <h1 className="page-title">Checkout</h1>
+        <div className="checkout-container">
+          <div className="checkout-items">
+            <h2>Order Summary</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
